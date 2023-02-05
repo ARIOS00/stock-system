f304783932355677bea014375f9159e4ea23b851
ARIOS00 <3588315821@qq.com>
2023-02-04 23:15:38 -0500

    service register interface

--- stock-services/stock-users/src/main/java/com/example/stock/service/IUserRegister.java
+++ stock-services/stock-users/src/main/java/com/example/stock/service/IUserRegister.java
@@ -1,4 +1,8 @@
 package com.example.stock.service;
 
+import com.example.stock.entity.User;
+import com.example.stock.vo.UserRegisterRequest;
+
 public interface IUserRegister {
+    User register(UserRegisterRequest request);
 }
--- stock-services/stock-users/src/main/java/com/example/stock/service/impl/UserRegisterImpl.java
+++ stock-services/stock-users/src/main/java/com/example/stock/service/impl/UserRegisterImpl.java
@@ -1,4 +1,5 @@
 package com.example.stock.service.impl;
 
+
 public class UserRegisterImpl {
 }
