3d90622bcac3b35f7a27d7e0707c6435d0be8004
ARIOS00 <3588315821@qq.com>
2023-02-08 15:12:11 -0500

    cookie redis bugs fixed

--- stock-services/stock-common/src/main/java/com/example/stock/users/GetUserFromCookie.java
+++ stock-services/stock-common/src/main/java/com/example/stock/users/GetUserFromCookie.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.example.stock.exception.UserException;
 import com.example.stock.vo.UserSDK;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -25,16 +26,11 @@ public class GetUserFromCookie {
             throw new UserException("cookie is empty!");
         log.info("try to get user info based on cookie: user:{}", cookie);
         Map<Object, Object> map =  redisTemplate.opsForHash().entries("user:" + cookie);
-        System.out.println("jjjj: " + map.get("registerDate"));
+        if(MapUtils.isEmpty(map))
+            throw new UserException("cannot find user info in redis based on cookie: user:" + cookie);
         UserSDK userSDK = new UserSDK();
         userSDK = userSDK.getUserSDK(map);
-        System.out.println("oooooo:" + userSDK.getId());
-        System.out.println("oooooo:" + userSDK.getNickname());
-        System.out.println("oooooo:" + userSDK.getLastLoginDate());
-        System.out.println("oooooo:" + userSDK.getRegisterDate());
-        System.out.println("oooooo:" + userSDK.getLoginCount());
-        if(null == userSDK)
-            throw new UserException("cannot find user based on cookie: user:" + cookie);
+
         log.info("current user is: {}", userSDK);
         return userSDK;
     }
--- stock-services/stock-users/src/main/java/com/example/stock/serialization/UserSerialize.java
+++ stock-services/stock-users/src/main/java/com/example/stock/serialization/UserSerialize.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class UserSerialize extends JsonSerializer<User> {
 
@@ -17,8 +18,11 @@ public class UserSerialize extends JsonSerializer<User> {
         jsonGenerator.writeStringField("nickname", user.getNickname());
         jsonGenerator.writeStringField("password", user.getPassword());
         jsonGenerator.writeStringField("salt", user.getSalt());
-        jsonGenerator.writeStringField("registerDate", user.getRegisterDate().toString());
-        jsonGenerator.writeStringField("lastLoginDate", user.getLastLoginDate().toString());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        jsonGenerator.writeStringField("registerDate", simpleDateFormat.format(user.getRegisterDate()));
+        jsonGenerator.writeStringField("lastLoginDate", simpleDateFormat.format(user.getLastLoginDate()));
+
         jsonGenerator.writeStringField("loginCount", user.getLoginCount().toString());
 
         jsonGenerator.writeEndObject();
