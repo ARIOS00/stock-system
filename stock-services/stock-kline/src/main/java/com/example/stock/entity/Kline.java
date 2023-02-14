4e7894dc6b62b2d3fc1e7703b3d6347e949af293
ARIOS00 <3588315821@qq.com>
2023-02-14 10:38:42 -0500

    kline map for redis

--- stock-services/stock-kline/src/main/java/com/example/stock/entity/Kline.java
+++ stock-services/stock-kline/src/main/java/com/example/stock/entity/Kline.java
@@ -1,6 +1,7 @@
 package com.example.stock.entity;
 
 import com.example.stock.serialization.KlineSerialize;
+import com.example.stock.vo.UserSDK;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,7 +10,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Data
@@ -43,4 +48,34 @@ public class Kline implements Serializable {
 
     @Column(name = "low", nullable = false)
     private Double low;
+
+    public Map<String, String> getMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", this.getName());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        map.put("kdate", simpleDateFormat.format(this.getKdate()));
+        map.put("close", this.getClose().toString());
+        map.put("volume", this.getVolume().toString());
+        map.put("open", this.getOpen().toString());
+        map.put("high", this.getHigh().toString());
+        map.put("low", this.getLow().toString());
+        return map;
+    }
+
+    public Kline getKline(Map<Object, Object> objMap) throws ParseException {
+        Map<String, String> map = new HashMap<>();
+        for (Map.Entry<Object, Object> entry : objMap.entrySet()) {
+            map.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+        }
+
+        this.setName(map.get("name"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        this.setKdate(formatter.parse(map.get("kdate")));
+        this.setClose(Double.parseDouble(map.get("close")));
+        this.setVolume(Double.parseDouble(map.get("volume")));
+        this.setOpen(Double.parseDouble(map.get("open")));
+        this.setHigh(Double.parseDouble(map.get("high")));
+        this.setLow(Double.parseDouble(map.get("low")));
+        return this;
+    }
 }
