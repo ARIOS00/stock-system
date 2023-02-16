146df3d85a9b33052e71ff9e11310e24ca6e1323
ARIOS00 <3588315821@qq.com>
2023-02-15 21:23:08 -0500

    kline interface initialized

--- stock-services/stock-kline/src/main/java/com/example/stock/service/IKlineCurveService.java
+++ stock-services/stock-kline/src/main/java/com/example/stock/service/IKlineCurveService.java
@@ -1,9 +1,15 @@
 package com.example.stock.service;
 
 import com.example.stock.entity.Kline;
+import com.example.stock.exception.KlineException;
 
 import java.util.Date;
+import java.util.List;
 
 public interface IKlineCurveService {
-    Kline getKlineByNameAndDate(String name, Date date);
+    Kline getKlineByNameAndDate(String name, Date date) throws KlineException;
+
+    List<Kline> getKlineCurveByName(String name) throws KlineException;
+
+    List<Kline> getKlineCurveByNameAndDuration(String name, Date startDate, Date endDate) throws KlineException;
 }
