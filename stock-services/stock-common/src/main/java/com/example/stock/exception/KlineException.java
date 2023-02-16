5db70361c61f81bc1016449efee23ce9c1031886
ARIOS00 <3588315821@qq.com>
2023-02-15 21:19:43 -0500

    kline exception definition

---
+++ stock-services/stock-common/src/main/java/com/example/stock/exception/KlineException.java
@@ -0,0 +1,7 @@
+package com.example.stock.exception;
+
+public class KlineException extends Exception{
+    public KlineException(String msg) {
+        super(msg);
+    }
+}
--- stock-services/stock-kline/src/main/java/com/example/stock/service/IKlineCurveService.java
+++ stock-services/stock-kline/src/main/java/com/example/stock/service/IKlineCurveService.java
@@ -1,4 +1,9 @@
 package com.example.stock.service;
 
+import com.example.stock.entity.Kline;
+
+import java.util.Date;
+
 public interface IKlineCurveService {
+    Kline getKlineByNameAndDate(String name, Date date);
 }
